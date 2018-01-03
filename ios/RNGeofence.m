f4f6d7cf2dd74f7bde12f2a1e59eef29b16668d5
Rasita Satianrapapong <rasita_sa@hotmail.com>
2018-01-03 18:19:40 +0700

    Support 20+ geofences in iOS

--- ios/RNGeofence.m
+++ ios/RNGeofence.m
@@ -3,8 +3,12 @@
 
 static NSString *const ENTER_GEOFENCE = @"RNGeofence:EnterGeofence";
 static NSString *const EXIT_GEOFENCE = @"RNGeofence:ExitGeofence";
+static int const maximumMonitoringRegionCount = 20;
+static NSString *const superRegionIdentifier = @"RNGeofence:SUPER";
+static NSString *const regionsKey = @"RNGeofence:regions";
 
 @implementation RNGeofence
+BOOL refreshing = false;
 
 - (dispatch_queue_t)methodQueue
 {
@@ -55,24 +59,50 @@ RCT_EXPORT_METHOD(addGeofence:(NSDictionary*)config) {
     if (self.locationManager == nil) {
         [NSException raise:@"Location Manager Not Initialized" format:@"You must initialize location manager first."];
     }
+    int savedRegionsCount = [self countSavedRegions];
     
-    CLRegion *region = [self mapDictionaryToRegion:config];
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [_locationManager startMonitoringForRegion:region];
-    });
+    CLCircularRegion *region = [self mapDictionaryToRegion:config];
+    [self saveRegion:region];
+    
+    if(savedRegionsCount + 1 <= maximumMonitoringRegionCount) {
+        NSLog(@"Number of fences is under the limit. Start monitoring normally.");
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [_locationManager startMonitoringForRegion:region];
+        });
+    } else {
+        // Over the limit. Create one dummy region and another (maximumMonitoringRegionCount - 1) regions that are closest to user's current location
+        NSLog(@"Number of fences is over the limit. Start getting location");
+        // If over the limit, get user location and let monitorNearbyRegions() handle the monitoring
+        refreshing = true;
+        [_locationManager startUpdatingLocation];
+    }
 }
 
 RCT_EXPORT_METHOD(addGeofences:(NSArray*) configs) {
     if (self.locationManager == nil) {
         [NSException raise:@"Location Manager Not Initialized" format:@"You must initialize location manager first."];
     }
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
-        for(NSDictionary *regionDict in configs) {
-            CLRegion *region = [self mapDictionaryToRegion:regionDict];
-            [_locationManager startMonitoringForRegion:region];
-        }
-    });
+    int savedRegionsCount = [self countSavedRegions];
+    NSMutableArray<CLCircularRegion *> *regionsToMonitor = [NSMutableArray array];
+    for(NSDictionary *regionDict in configs) {
+        CLCircularRegion *region = [self mapDictionaryToRegion:regionDict];
+        [regionsToMonitor addObject:region];
+    }
+    [self saveRegions:regionsToMonitor];
+    if(savedRegionsCount + configs.count <= maximumMonitoringRegionCount) {
+        NSLog(@"Number of fences is under the limit. Start monitoring normally.");
+        dispatch_async(dispatch_get_main_queue(), ^{
+            for(CLCircularRegion *region in regionsToMonitor) {
+                [_locationManager startMonitoringForRegion:region];
+            }
+        });
+    } else {
+        // Over the limit. Create one dummy region and another (maximumMonitoringRegionCount - 1) regions that are closest to user's current location
+        NSLog(@"Number of fences is over the limit. Start getting location");
+        // If over the limit, get user location and let monitorNearbyRegions() handle the monitoring
+        refreshing = true;
+        [_locationManager startUpdatingLocation];
+    }
 }
 
 RCT_EXPORT_METHOD(removeGeofence:(NSString*)identifier) {
@@ -82,8 +112,17 @@ RCT_EXPORT_METHOD(removeGeofence:(NSString*)identifier) {
     RCTLogInfo(@"Remove geofence = %@",identifier);
     for(CLRegion *region in [_locationManager monitoredRegions]) {
         if([region.identifier isEqualToString:identifier]) {
+            int savedRegionsCount = [self countSavedRegions];
             dispatch_async(dispatch_get_main_queue(), ^{
                 [_locationManager stopMonitoringForRegion: region];
+                [self deleteSavedRegion:region.identifier];
+                if(savedRegionsCount - 1 > maximumMonitoringRegionCount) {
+                    // Over the limit. Create one dummy region and another (maximumMonitoringRegionCount - 1) regions that are closest to user's current location
+                    NSLog(@"Number of fences is over the limit. Start getting location");
+                    // If over the limit, get user location and let monitorNearbyRegions() handle the monitoring
+                    refreshing = true;
+                    [_locationManager startUpdatingLocation];
+                }
                 return;
             });
         }
@@ -98,6 +137,7 @@ RCT_EXPORT_METHOD(removeAllGeofences) {
     dispatch_async(dispatch_get_main_queue(), ^{
         for(CLRegion *region in [_locationManager monitoredRegions]) {
             [_locationManager stopMonitoringForRegion: region];
+            [self deleteSavedRegion:region.identifier];
         }
     });
 }
@@ -139,7 +179,13 @@ RCT_EXPORT_METHOD(locationServicesEnabled:(RCTPromiseResolveBlock)resolve
 - (void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region
 {
     RCTLogInfo(@"Exit geofence %@", region.identifier);
-    [self emitMessageToRN:EXIT_GEOFENCE :@{@"identifier": region.identifier}];
+    if([region.identifier isEqualToString:superRegionIdentifier]) {
+        NSLog(@"Exit super geofence. Refreshing geofences");
+        refreshing = true;
+        [_locationManager startUpdatingLocation];
+    } else {
+        [self emitMessageToRN:EXIT_GEOFENCE :@{@"identifier": region.identifier}];
+    }
 }
 - (void)locationManager:(CLLocationManager *)manager didStartMonitoringForRegion:(CLRegion *)region
 {
@@ -149,5 +195,183 @@ RCT_EXPORT_METHOD(locationServicesEnabled:(RCTPromiseResolveBlock)resolve
 {
     RCTLogInfo(@"Monitoring geofence %@ failed: %@", region.identifier, error.description);
 }
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations
+{
+    NSLog(@"User locations = %@",locations);
+    // If refreshing geofences, forward acquired location to geofence selection process
+    if(refreshing) {
+        refreshing = false;
+        [_locationManager stopUpdatingLocation];
+        CLLocation *userLocation = locations.lastObject;
+        [self monitorNearbyRegions:userLocation];
+    }
+}
+
+//
+/**
+ Start monitor as many geofences as permitted by iOS. Only for 20+ geofences
+ Will monitor [maximumMonitoringRegionCount-1] geofences + 1 super geofence
+ */
+- (void)monitorNearbyRegions:(CLLocation *)userLocation
+{
+    // Stop monitoring all geofences
+    for(CLRegion *region in [_locationManager monitoredRegions]) {
+        [_locationManager stopMonitoringForRegion: region];
+    }
+    // Retrieve from saved
+    NSArray<CLCircularRegion *> *regionsToMonitor = [self restoreAllRegions];
+    // Sort geofences by distance from user location
+    NSArray<CLCircularRegion *> *sortedRegions = [self sortRegions:regionsToMonitor byLocation:userLocation];
+    // Truncate to [maximumMonitoringRegionCount-1] geofences
+    NSArray<CLCircularRegion *> *nearbyRegions = [sortedRegions subarrayWithRange:NSMakeRange(0, maximumMonitoringRegionCount-1)];
+    // Start monitoring each geofences
+    for(CLCircularRegion *region in nearbyRegions) {
+        CLLocation *regionCenter = [[CLLocation alloc] initWithLatitude:region.center.latitude longitude:region.center.longitude];
+        NSLog(@"Monitor region %@, distance = %lf", region.identifier, [userLocation distanceFromLocation:regionCenter]);
+        [_locationManager startMonitoringForRegion:region];
+        [self saveRegion:region];
+    }
+    // Create and monitor "super geofence"
+    CLCircularRegion *farthestRegion = [nearbyRegions lastObject];
+    if(farthestRegion != nil) {
+        CLCircularRegion *superRegion = [self createSuperRegion:farthestRegion fromLocation:userLocation];
+        [_locationManager startMonitoringForRegion:superRegion];
+    }
+}
+
+/**
+ Sort geofences by distance ascendingly
+ */
+- (NSArray<CLCircularRegion *> *)sortRegions: (NSArray<CLCircularRegion *> *)regions byLocation:(CLLocation *)location
+{
+    return [regions sortedArrayUsingComparator:^NSComparisonResult(CLCircularRegion *region1, CLCircularRegion *region2){
+        CLLocation *region1Center = [[CLLocation alloc] initWithLatitude:region1.center.latitude longitude:region1.center.longitude];
+        CLLocation *region2Center = [[CLLocation alloc] initWithLatitude:region2.center.latitude longitude:region2.center.longitude];
+        CLLocationDistance distance1 = [location distanceFromLocation:region1Center];
+        CLLocationDistance distance2 = [location distanceFromLocation:region2Center];
+        if(distance1 < distance2) {
+            return (NSComparisonResult)NSOrderedAscending;
+        } else if(distance1 > distance2) {
+            return (NSComparisonResult)NSOrderedDescending;
+        } else {
+            return (NSComparisonResult)NSOrderedSame;
+        }
+    }];
+}
+
+/**
+ Create "super geofence" (special geofence which covers the nearest [monitoringLimit - 1] geofences)
+ */
+- (CLCircularRegion *)createSuperRegion: (CLCircularRegion *)farthestRegion fromLocation:(CLLocation *)userLocation
+{
+    // radius = distance from user location to the farthest region's nearer rim
+    CLLocation *farthestRegionCenter = [[CLLocation alloc] initWithLatitude:farthestRegion.center.latitude longitude:farthestRegion.center.longitude];
+    CLLocationDistance superRegionRadius = [userLocation distanceFromLocation:farthestRegionCenter] - farthestRegion.radius;
+    CLLocationDistance clampedRadius = MIN(superRegionRadius, _locationManager.maximumRegionMonitoringDistance);
+    CLCircularRegion *superRegion = [[CLCircularRegion alloc] initWithCenter: userLocation.coordinate
+                                                                      radius: clampedRadius
+                                                                  identifier: superRegionIdentifier];
+    // Notify on exit only
+    superRegion.notifyOnEntry = false;
+    superRegion.notifyOnExit = true;
+    return superRegion;
+}
+
+// MARK: - Persistence
+
+/**
+ Persist a region in NSUserDefaults
+ */
+- (void)saveRegion: (CLCircularRegion *)region
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSMutableDictionary *regionsDict = [[defaults objectForKey:regionsKey] mutableCopy];
+    if(regionsDict == nil) {
+        regionsDict = [NSMutableDictionary dictionary];
+    }
+    NSDictionary *regionDict = @{ @"identifier": region.identifier,
+                                  @"latitude": [NSNumber numberWithDouble:region.center.latitude],
+                                  @"longitude": [NSNumber numberWithDouble:region.center.longitude],
+                                  @"radius": [NSNumber numberWithDouble:region.radius]
+                                  };
+    [regionsDict setObject:regionDict forKey:region.identifier];
+    [defaults setObject:regionsDict forKey:regionsKey];
+    [defaults synchronize];
+}
+
+/**
+ Persist regions in NSUserDefaults
+ */
+- (void)saveRegions: (NSArray<CLCircularRegion *> *)regions
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSMutableDictionary *regionsDict = [[defaults objectForKey:regionsKey] mutableCopy];
+    if(regionsDict == nil) {
+        regionsDict = [NSMutableDictionary dictionary];
+    }
+    for(CLCircularRegion *region in regions) {
+        NSDictionary *regionDict = @{ @"identifier": region.identifier,
+                                      @"latitude": [NSNumber numberWithDouble:region.center.latitude],
+                                      @"longitude": [NSNumber numberWithDouble:region.center.longitude],
+                                      @"radius": [NSNumber numberWithDouble:region.radius]
+                                      };
+        [regionsDict setObject:regionDict forKey:region.identifier];
+    }
+    [defaults setObject:regionsDict forKey:regionsKey];
+    [defaults synchronize];
+}
+
+
+/**
+ Get all persisted regions from NSUserDefaults
+ */
+- (NSArray<CLCircularRegion *> *)restoreAllRegions
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSMutableDictionary *regionsDict = [defaults objectForKey:regionsKey];
+    if(regionsDict == nil) {
+        return [NSArray array];
+    }
+    NSMutableArray<CLCircularRegion *> *regions = [NSMutableArray array];
+    for(id key in regionsDict) {
+        NSDictionary *regionDict = [regionsDict objectForKey:key];
+        CLLocationCoordinate2D centerCoordinate = CLLocationCoordinate2DMake([[regionDict valueForKey:@"latitude"] doubleValue], [[regionDict valueForKey:@"longitude"] doubleValue]);
+        CLLocationDistance regionRadius = [[regionDict valueForKey:@"radius"] doubleValue];
+        CLCircularRegion *region = [[CLCircularRegion alloc] initWithCenter:centerCoordinate
+                                                                     radius: regionRadius
+                                                                 identifier: [regionDict valueForKey:@"identifier"]];
+        [regions addObject:region];
+    }
+    return regions;
+}
+
+/**
+ Delete persisted region from NSUserDefaults
+ */
+- (void)deleteSavedRegion: (NSString *)identifier
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSMutableDictionary *regionsDict = [[defaults objectForKey:regionsKey] mutableCopy];
+    if(regionsDict != nil) {
+        [regionsDict removeObjectForKey:identifier];
+    }
+    [defaults setObject:regionsDict forKey:regionsKey];
+    [defaults synchronize];
+}
+
+/**
+ Count number of saved regions
+ */
+- (int)countSavedRegions
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSMutableDictionary *regionsDict = [defaults objectForKey:regionsKey];
+    if(regionsDict == nil) {
+        return 0;
+    } else {
+        return (int)[regionsDict count];
+    }
+}
+
 @end
   
